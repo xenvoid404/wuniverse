5a7c34711195f29f0029b164129c0ebc7f212488
Xenvoid <xenvoid404@gmail.com>
2025-07-29 11:54:43 +0000

    perbaikan next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    // Options here
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'upload.wikimedia.org'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
